2f72c9f8bbfa027f9b2de945a1724bfc2d6358b8
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-07-23 12:11:25 +0600

    feat(service): create file service

---
+++ src/main/java/com/ecommerce/sb_ecom/service/FileServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ecommerce.sb_ecom.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String uploadImage(String path, MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String randomId = UUID.randomUUID().toString();
+        assert originalFilename != null;
+        String fileName = randomId.concat(originalFilename.substring(originalFilename.lastIndexOf('.')));
+        String filePath = path + File.pathSeparator + fileName;
+
+        File folder = new File(path);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+
+        return fileName;
+
+    }
+
+}
